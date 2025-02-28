4cf1fa874aa7c3d0727e1e7b025d0b63c48de276
Mateo Manchola <mmanchola12temporal@gmail.com>
2025-02-28 01:35:23 +0100

    All Menu methods created

---
+++ src/app/menu/Menu.java
@@ -0,0 +1,62 @@
+package app.menu;
+
+import java.util.ArrayList;
+
+import app.models.vehiculos.Vehiculo;
+
+public class Menu {
+
+    public static int showMenu() {
+        String[] options = {
+                "Crear Vehiculo",
+                "Mostrar Vehiculos",
+                "Realizar Mantenimiento" };
+
+        System.out.println("Menu");
+
+        for (int i = 0; i < options.length; i++) {
+            System.out.printf("%n\t%d - %s", i + 1, options[i]);
+        }
+
+        System.out.printf("%n\t%d - Salir", options.length + 1);
+
+        return options.length + 1;
+    }
+
+    public static int showCreateVehiculoMenu() {
+        String[] options = {
+                "Crear Coche",
+                "Crear Moto" };
+
+        System.out.println("Crear Vehiculo");
+
+        for (int i = 0; i < options.length; i++) {
+            System.out.printf("%n\t%d - %s", i + 1, options[i]);
+        }
+
+        System.out.printf("%n\t%d - volver", options.length + 1);
+
+        return options.length + 1;
+    }
+
+    public static void showVehiculos(ArrayList<Vehiculo> vehiculos) {
+        System.out.println("Mostrar Vehiculos");
+
+        for (Vehiculo vehiculo : vehiculos) {
+            System.out.println(vehiculo);
+        }
+    }
+
+    public static void showVehiculosPlates(ArrayList<Vehiculo> vehiculos) {
+        System.out.println("Mostrar Vehiculos");
+
+        for (Vehiculo vehiculo : vehiculos) {
+            System.out.println(vehiculo.getMatricula());
+        }
+    }
+
+    public static void clearMenu() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+}
